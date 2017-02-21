b006c29035ff17a36341cc0621766db69aa3c116
ivanlyutko <sholazar2@gmail.com>
2017-02-21 17:37:12 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
